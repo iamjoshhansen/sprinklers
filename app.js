17a3c5af31608dea8495c5e12d7fcfc121d9820d
Joshua Hansen <hansenlj@us.ibm.com>
2017-06-06 18:35:06 -0600

    Added test script to see if light will blink on GPIO 18

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello Sprinklers!!!!!!");
+console.log("All the Sprinklers!!!!1!!");
+
+var Gpio = require('onoff').Gpio,
+	led = new Gpio(18, 'out'),
+	val = false;
+
+setInterval(function () {
+
+	val = ! val;
+
+	console.log('writing value: ' + val);
+	led.writeSync(val ? 1 : 0);
+
+}, 1000);
