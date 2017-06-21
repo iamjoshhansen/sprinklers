c4100c704c88e6c3f58e1b570d4c0960f1939f5b
Joshua Hansen <hansenlj@us.ibm.com>
2017-06-20 18:07:56 -0600

    Inverted the Relay on/off signals. Relay construction now shuts all pins OFF to start.

--- components/relay.js
+++ components/relay.js
@@ -1,5 +1,8 @@
-var Gpio = require('onoff').Gpio,
-	_ = require('lodash');
+var Gpio   = require('onoff').Gpio,
+	_      = require('lodash'),
+	server = require('http').createServer(),
+	ON  = 0,
+	OFF = 1;
 
 module.exports = Relay;
 
@@ -27,50 +30,65 @@ function Relay (pins) {
 		}
 	});
 
+	_.each(this._outs, function (pin, label) {
+		console.log('Initiating to off: `' + label + '`');
+		pin.writeSync(OFF);
+	});
+
+	console.log('Relay construction complete.');
+
 }
 
-Relay.prototype.getActive = function () {
 
-	return this._active_index;
+_.extend(Relay.prototype, {
 
-};
 
-Relay.prototype.setActive = function (zone) {
+	getActive: function () {
+		return this._active_index;
+	},
 
-	if (this._active_index !== null) {
-		console.log('Turning off zone ' + this._active_index);
-		this._outs[this._active_index].writeSync(0);
-	}
 
-	this._active_index = zone;
 
-	if (zone !== null) {
+	setActive: function (zone) {
 
-		if (zone < 0) {
-			throw new Error('zone cannot be < 0');
+		if (this._active_index !== null) {
+			console.log('Turning off zone ' + this._active_index);
+			this._outs[this._active_index].writeSync(ON);
 		}
 
-		if (zone > this.length - 1) {
-			throw new Error('zone cannot be beyond the range of pins (' + (this.length - 1) + ')');
+		this._active_index = zone;
+
+		if (zone !== null) {
+
+			if (zone < 0) {
+				throw new Error('zone cannot be < 0');
+			}
+
+			if (zone > this.length - 1) {
+				throw new Error('zone cannot be beyond the range of pins (' + (this.length - 1) + ')');
+			}
+
+			console.log('Turning on zone ' + this._active_index);
+			this._outs[this._active_index].writeSync(OFF);
 		}
 
-		console.log('Turning on zone ' + this._active_index);
-		this._outs[this._active_index].writeSync(1);
+		return this;
+
+	},
+
+
+	next: function () {
+
+		if (this._active_index === null) {
+			this.setActive(0);
+		} else {
+			this.setActive( (this._active_index+1) % this.length );
+		}
+
+		return this;
+
 	}
 
-	return this;
-
-};
 
 
-Relay.prototype.next = function () {
-
-	if (this._active_index === null) {
-		this.setActive(0);
-	} else {
-		this.setActive( (this._active_index+1) % this.length );
-	}
-
-	return this;
-
-};
+});
