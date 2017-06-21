aae6768947ab1cbf105a4cbad139943acc01e9bd
Joshua Hansen <hansenlj@us.ibm.com>
2017-06-20 22:48:13 -0600

    Added a green LED indicator for when there is at least one connection.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 console.log("All the Sprinklers!!!!1!!");
 
+var Gpio  = require('onoff').Gpio;
 var Relay = require('./components/relay.js');
 var relay = new Relay([12,16,20,21]);
 
@@ -8,11 +9,20 @@ var relay = new Relay([12,16,20,21]);
 // }, 1000);
 
 
+var green_button = new Gpio(18, 'out');
+
+
+
 var port = 2235;
 var io = require('socket.io')(port);
 
+var connection_count = 0;
+
 io.on('connection', function (socket) {
 	console.log('We have a new connection!');
+
+	connection_count++;
+	green_button.writeSync(0);
 
 	socket.on('activate-zone', function (zone_index) {
 		console.log('Activating zone ', zone_index);
@@ -21,6 +31,11 @@ io.on('connection', function (socket) {
 
 	socket.on('disconnect', function () {
 		console.log('user disconnected');
+		connection_count--;
+
+		if (connection_count < 1) {
+			green_button.writeSync(1);
+		}
 	});
 });
 
